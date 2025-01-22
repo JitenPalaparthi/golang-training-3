0f764a82bdc3d4fdafc4e7aee1076bab5d3250dc
Jiten <jpalaparthi@gmail.com>
2025-01-22 10:45:27 +0530

    add struct as a variable

--- 31-udt-struct-as-variable/main.go
+++ 31-udt-struct-as-variable/main.go
@@ -3,13 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	var p1 struct{ Name, Email string } = struct {
-		Name  string
-		Email string
-	}{Name: "Jiten", Email: "Jitenp@outlook.com"}
+	var p1 struct {
+		Name, Email string
+		Address     struct{ City string }
+	} = struct {
+		Name    string
+		Email   string
+		Address struct{ City string }
+	}{Name: "Jiten", Email: "Jitenp@outlook.com",
+		Address: struct{ City string }{City: "Mumbai"}}
 
-	p1.Email = "Jiten.Palaparthi@Gmail.Com"
-	fmt.Println(p1)
+	PrintDetails(p1)
+
+	var p2 struct {
+		Name, Email string
+		Address     struct{ City string }
+	}
+
+	p2.Name = "Priya"
+	p2.Email = "Priya@Outlook.com"
+	p2.Address.City = "Bengalore"
+	PrintDetails(p2)
 }
 
-// func PrintDetails(p )
+func PrintDetails(p struct {
+	Name, Email string
+	Address     struct{ City string }
+}) {
+	fmt.Println("Name:", p.Name)
+	fmt.Println("Email:", p.Email)
+	fmt.Println("City:", p.Address.City)
+}
+
+// type Person struct {
+// 	Name, Email string
+// }
